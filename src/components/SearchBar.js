240f6382fd8338b982a71de3e002b092261aba61
valentin.pinte <valentin.pinte@etu.imt-lille-douai.fr>
2023-08-04 09:28:53 +0200

    Adding searchscreen + searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+    return (
+        <View style={styles.background}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Rechercher"
+                style={styles.inputStyle}
+                value={term}
+                onChange={newTerm => onTermChange(newTerm.nativeEvent.text)}
+                onEndEditing={() => onTermSubmit()}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'lightblue',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        marginTop: 10,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 30,
+        alignSelf: 'center'
+    }
+});
+
+export default SearchBar;
